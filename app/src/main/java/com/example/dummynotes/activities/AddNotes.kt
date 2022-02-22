0f090ea8b390e5186d965707b315346a132b75c3
Rohit <suriyarohit1995@gmail.com>
2022-02-22 22:54:26 +0530

    * Multiple minor changes

--- app/src/main/java/com/example/dummynotes/activities/AddNotes.kt
+++ app/src/main/java/com/example/dummynotes/activities/AddNotes.kt
@@ -1,14 +1,15 @@
 package com.example.dummynotes.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.dummynotes.database.NotesEntity
 import com.example.dummynotes.databinding.ActivityAddNotesBinding
 import com.example.dummynotes.viewModels.NotesViewModel
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.slider.LabelFormatter
+
 
 class AddNotes : AppCompatActivity() {
 
@@ -17,8 +18,12 @@ class AddNotes : AppCompatActivity() {
     private lateinit var notesString: String
     private lateinit var titleString: String
     private lateinit var positionID: String
+    private var priorityLevelCount = 1.0f
+    val priorityLevel = "Priority Level : "
 
 
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,6 +36,7 @@ class AddNotes : AppCompatActivity() {
             notesString = intent.getStringExtra("message")?:"no data"
             titleString = intent.getStringExtra("title")?:"no data"
             positionID = intent.getStringExtra("id")?:"new"
+//            priorityLevelCount = intent.getFloatExtra("priority")?:0.0f
 
             if (!positionID.equals("new")) {
                 addNotesBinding.title.setText(titleString)
@@ -41,10 +47,36 @@ class AddNotes : AppCompatActivity() {
                 addNotes()
             }
 
+            addNotesBinding.priorityText.text = priorityLevel+"Low & not important"
+            addNotesBinding.prioritySlider.addOnChangeListener { slider, value, fromUser ->
+                setPriorityText(addNotesBinding.prioritySlider.value)
+            }
+
+            if (priorityLevelCount>1.0f){
+                if(priorityLevelCount == 2.0f){
+                    addNotesBinding.prioritySlider.value = 2.0f
+                }else if(priorityLevelCount == 3.0f){
+                    addNotesBinding.prioritySlider.value = 3.0f
+                }else if(priorityLevelCount == 4.0f){
+                    addNotesBinding.prioritySlider.value = 4.0f
+                }
+                setPriorityText(priorityLevelCount)
+            }
+
+
         } catch (e: Exception) {
             e.printStackTrace()
         }
 
+    }
+
+    private fun setPriorityText(value : Float){
+        when(value){
+            1.0f -> addNotesBinding.priorityText.text = priorityLevel+"Low priority & not important"
+            2.0f -> addNotesBinding.priorityText.text = priorityLevel+"Low priority & important"
+            3.0f -> addNotesBinding.priorityText.text = priorityLevel+"High priority & not important"
+            4.0f -> addNotesBinding.priorityText.text = priorityLevel+"High priority & important"
+        }
     }
 
     private fun addNotes(){
@@ -66,6 +98,8 @@ class AddNotes : AppCompatActivity() {
                         Toast.makeText(baseContext,"Updated Successfully", Toast.LENGTH_SHORT).show()
 
                     }
+
+                    finish()
 
                 } else {
                     Toast.makeText(this, "Enter Description", Toast.LENGTH_SHORT).show()
